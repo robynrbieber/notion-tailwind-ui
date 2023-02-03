ef400cfed692fd51a5848afe61e52b33005fa7f6
robynrbieber <61341377+robynrbieber@users.noreply.github.com>
2023-02-02 22:16:18 -0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,22 @@
-//
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  theme: {
+    colors: {
+      'black': {
+        100: '#E6E4E1',
+        200: '#CCCAC3',
+        300: '#ABA79D',
+        400: '#898578',
+        500: '#6F6B5D',
+        600: '#585449',
+        700: '#48463D',
+        800: '#3C3A33',
+        900: '#292824',
+    },
+  fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+  },
+}
